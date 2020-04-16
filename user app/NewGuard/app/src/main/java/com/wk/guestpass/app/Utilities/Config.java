e54507a72151fe528275ba40dbb398de5cca6621
maninder <maninderchalotra@gmail.com>
2020-04-16 18:23:20 +0530

    user App

---
+++ user app/NewGuard/app/src/main/java/com/wk/guestpass/app/Utilities/Config.java
@@ -0,0 +1,53 @@
+package com.wk.guestpass.app.Utilities;
+
+public class Config {
+   /* public static final String login="http://sagarfoundation.org.in/guestpass/Api/user_login";
+    public static final String todayslist="http://sagarfoundation.org.in/guestpass/Api/todays_guest";
+    public static final String upcominglist="http://sagarfoundation.org.in/guestpass/Api/upcoming_guest";
+    public static final String homelist="http://sagarfoundation.org.in/guestpass/Api/home_list";
+    public static final String addguest="http://sagarfoundation.org.in/guestpass/Api/add_guest";
+    public static final String guestcheck="http://sagarfoundation.org.in/guestpass/Api/fetch_guest";
+    public static final String deleteguest="http://sagarfoundation.org.in/guestpass/Api/cancel";
+    public static final String splashscreen="http://sagarfoundation.org.in/guestpass/Api/splash";
+    public static final String refcodes="http://sagarfoundation.org.in/guestpass/Api/fetch_user_details";
+    public static final String signup="http://sagarfoundation.org.in/guestpass/Api/signup";
+    public static final String help="http://sagarfoundation.org.in/guestpass/Api/help";*/
+
+    public static final String login="http://qentry.xyz/Api/user_login";
+    public static final String todayslist="http://qentry.xyz/Api/todays_guest";
+    public static final String upcominglist="http://qentry.xyz/Api/upcoming_guest";
+    public static final String homelist="http://qentry.xyz/Api/home_list";
+    public static final String addguest="http://qentry.xyz/Api/add_guest";
+    public static final String guestcheck="http://qentry.xyz/Api/fetch_guest";
+    public static final String deleteguest="http://qentry.xyz/Api/cancel";
+    public static final String splashscreen="http://qentry.xyz/Api/splash";
+    public static final String refcodes="http://qentry.xyz/Api/fetch_user_details";
+    public static final String signup="http://qentry.xyz/Api/signup";
+    public static final String help="http://qentry.xyz/Api/help";
+
+}
+/*  public static final String login="http://18.221.138.210/guestpass/api/user_login";
+    public static final String todayslist="http://18.221.138.210/guestpass/api/todays_guest";
+    public static final String upcominglist="http://18.221.138.210/guestpass/api/upcoming_guest";
+    public static final String homelist="http://18.221.138.210/guestpass/api/home_list";
+    public static final String addguest="http://18.221.138.210/guestpass/api/add_guest";
+    public static final String guestcheck="http://18.221.138.210/guestpass/api/fetch_guest";
+    public static final String deleteguest="http://18.221.138.210/guestpass/api/cancel";
+    public static final String splashscreen="http://18.221.138.210/guestpass/api/splash";
+    public static final String refcodes="http://18.221.138.210/guestpass/api/fetch_user_details";
+    public static final String signup="http://18.221.138.210/guestpass/api/signup";
+    public static final String help="http://18.221.138.210/guestpass/api/help";     */
+
+
+
+/*   public static final String login="http://sagarfoundation.org.in/guestpass/Api/user_login";
+    public static final String todayslist="http://sagarfoundation.org.in/guestpass/Api/todays_guest";
+    public static final String upcominglist="http://sagarfoundation.org.in/guestpass/Api/upcoming_guest";
+    public static final String homelist="http://sagarfoundation.org.in/guestpass/Api/home_list";
+    public static final String addguest="http://sagarfoundation.org.in/guestpass/Api/add_guest";
+    public static final String guestcheck="http://sagarfoundation.org.in/guestpass/Api/fetch_guest";
+    public static final String deleteguest="http://sagarfoundation.org.in/guestpass/Api/cancel";
+    public static final String splashscreen="http://sagarfoundation.org.in/guestpass/Api/splash";
+    public static final String refcodes="http://sagarfoundation.org.in/guestpass/Api/fetch_user_details";
+    public static final String signup="http://sagarfoundation.org.in/guestpass/Api/signup";
+    public static final String help="http://sagarfoundation.org.in/guestpass/Api/help";  */
