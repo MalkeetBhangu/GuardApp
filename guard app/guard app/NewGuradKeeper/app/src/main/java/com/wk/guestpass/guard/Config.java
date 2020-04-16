8284a4cae995db732fde5e28eec6640061d52520
maninder <maninderchalotra@gmail.com>
2020-04-16 18:05:42 +0530

    Guard App

---
+++ guard app/guard app/NewGuradKeeper/app/src/main/java/com/wk/guestpass/guard/Config.java
@@ -0,0 +1,28 @@
+package com.wk.guestpass.guard;
+
+public class Config {
+
+    /*public static final String checkcode="http://18.221.138.210/guestpass/api/check_code";
+    public static final String login="http://18.221.138.210/guestpass/api/guard_login";
+    public static final String dashboard="http://18.221.138.210/guestpass/api/total_visitors";
+    public static final String checkin="http://18.221.138.210/guestpass/api/guest_checkin";
+    public static final String splashscreen="http://18.221.138.210/guestpass/api/splash";*/
+
+    public static final String checkcode="http://qentry.xyz/Api/check_code";
+    public static final String login="http://qentry.xyz/Api/guard_login";
+    public static final String dashboard="http://qentry.xyz/Api/total_visitors";
+    public static final String checkin="http://qentry.xyz/Api/guest_checkin";
+    public static final String splashscreen="http://qentry.xyz/Api/splash";
+}
+/*  public static final String checkcode="http://18.221.138.210/guestpass/api/check_code";
+    public static final String login="http://18.221.138.210/guestpass/api/guard_login";
+    public static final String dashboard="http://18.221.138.210/guestpass/api/total_visitors";
+    public static final String checkin="http://18.221.138.210/guestpass/api/guest_checkin";
+    public static final String splashscreen="http://18.221.138.210/guestpass/api/splash"; */
+
+
+/*  public static final String checkcode="http://sagarfoundation.org.in/guestpass/api/check_code";
+    public static final String login="http://sagarfoundation.org.in/guestpass/api/guard_login";
+    public static final String dashboard="http://sagarfoundation.org.in/guestpass/api/total_visitors";
+    public static final String checkin="http://sagarfoundation.org.in/guestpass/api/guest_checkin";
+    public static final String splashscreen="http://sagarfoundation.org.in/guestpass/api/splash"; */
